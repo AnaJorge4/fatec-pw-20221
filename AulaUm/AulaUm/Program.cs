3fb69d49af58351e4b9d69723ea57fe7a9c8767b
Ana Lívia Jorge <79546581+AnaJorge4@users.noreply.github.com>
2022-02-11 21:51:56 -0300

    Add files via upload

---
+++ AulaUm/AulaUm/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaUm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /* IF */
+            if(10 < 12)
+            {
+                Console.WriteLine("Verdadeiro");
+            }
+
+            else
+            {
+                Console.WriteLine("Falso");
+            }
+
+            /* For */
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+
+            while (true)
+            {
+
+            }
+
+            foreach (var item in collection)
+            {
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
